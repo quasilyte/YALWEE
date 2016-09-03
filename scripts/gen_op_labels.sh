a38505487cbf5e41c201bda5f389d06ade4f8287
quasilyte <quasilyte@gmail.com>
2016-09-04 01:34:13 +0300

    now generating op labels with encoding info

--- scripts/gen_op_labels.sh
+++ scripts/gen_op_labels.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ruby ./scripts/gen_op_enum.rb |
-    awk 'BEGIN { print ";; this file was generated, do not edit by hand"} 
-         {print "dq @@op_"$0}' > \
+    awk 'BEGIN { 
+           print ";; this file was generated, do not edit by hand"
+         } 
+         { print "dq @@op_"$0 " ;; " count++ }' > \
         src/op_labels.s
           
