c880f0ac2137c940b856509103b2f60dd6e95db7
quasilyte <quasilyte@gmail.com>
2016-09-05 00:57:36 +0300

    mov R->R opcodes

--- src/ops/fill.s
+++ src/ops/fill.s
@@ -6,6 +6,14 @@
   @each_r @op_fill_counter_by_r?
   @each_imm @op_fill_counter_by_imm?
   @each @op_fill_r_x?_by_0, 3, 4, 5, 6, 7, 8
+  @each_curry @op_fill_r?_by_r?, 0,  1, 2, 3, 4, 5, 6, 7
+  @each_curry @op_fill_r?_by_r?, 1,  0, 2, 3, 4, 5, 6, 7
+  @each_curry @op_fill_r?_by_r?, 2,  0, 1, 3, 4, 5, 6, 7
+  @each_curry @op_fill_r?_by_r?, 3,  0, 1, 2, 4, 5, 6, 7
+  @each_curry @op_fill_r?_by_r?, 4,  0, 1, 2, 3, 5, 6, 7
+  @each_curry @op_fill_r?_by_r?, 5,  0, 1, 2, 3, 4, 6, 7
+  @each_curry @op_fill_r?_by_r?, 6,  0, 1, 2, 3, 4, 5, 7
+  @each_curry @op_fill_r?_by_r?, 7,  0, 1, 2, 3, 4, 5, 6
 %endmacro
 
 %macro @op_fill_r?_by_0 1
@@ -54,5 +62,23 @@
       %assign i i-1
     %endrep
 
+    @next_op
+%endmacro
+
+%macro @op_fill_r?_by_r? 2
+  @@op_fill_r%1_by_r%2:
+    mov R%1, R%2
+    @next_op
+%endmacro
+
+%macro @op_fill_r0_by_r? 1
+  @@op_fill_r0_by_r%1:
+    mov R0, R%1
+    @next_op
+%endmacro
+
+%macro @op_fill_r1_by_r? 1
+  @@op_fill_r1_by_r%1:
+    mov R1, R%1
     @next_op
 %endmacro
