60984a427fd3ba3bd585fc894fcee4ffa3b01f68
quasilyte <quasilyte@gmail.com>
2016-09-04 12:59:03 +0300

    decomposing scripts

---
+++ scripts/gen_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+op_enum_path='tmp/op_enum.txt'
+s_labels='src/op_labels.s'
+c_mnemonics='src/c_api/mnemonics.h'
+
+ruby ./scripts/gen_op_enum.rb > $op_enum_path
+
+echo "generating $s_labels"
+awk -f scripts/op_enum_to_labels.awk "$op_enum_path" > "$s_labels"
+
+echo "generating $c_mnemonics"
+awk -f scripts/op_enum_to_mnemonics.awk "$op_enum_path" > "$c_mnemonics"
+          
+echo 'done'
+rm "$op_enum_path"
---
+++ scripts/op_enum_to_labels.awk
@@ -0,0 +1,5 @@
+BEGIN { 
+  print ";; this file was generated, do not edit by hand"
+} 
+
+{ print "dq @@op_"$0 " ;; " count++ }
