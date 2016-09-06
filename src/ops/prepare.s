332702794e702d14047067f65bb81cbcbaf4c30a
quasilyte <quasilyte@gmail.com>
2016-09-06 21:20:20 +0300

    max byte size for JIT'able opcode is now 16

--- src/ops/prepare.s
+++ src/ops/prepare.s
@@ -1,19 +1,21 @@
 %macro @prepare_ops 0
-  @expand_list_range @op_prepare?_e?, 0, E_COUNT, 4, 8
+  @expand_list_range @op_prepare?_e?, 0, E_COUNT, 4, 8, 12, 16
 %endmacro
 
 %macro @op_prepare?_e? 2
   @@op_prepare%1_e%2:
-    xor rcx, rcx ;; 0 offset
+    xor rcx, rcx
 
     ;; copying machine code from labels
     %assign %%i 0
     %rep %1
       movzx rax, word [rdi+(2 * %%i)] ;; opcode
+
       mov rdx, [$op_sizes+(rax * 8)] ;; code size
       mov rax, [$op_labels+(rax*8)]  ;; code address
-      mov rax, [rax]                 ;; code
-      mov [$e%2 + rcx], eax          ;; copy code 
+
+      movdqu xmm8, [rax]             ;; code
+      movdqu [$e%2 + rcx], xmm8      ;; copy code 
       add rcx, rdx                   ;; save offset
 
       %assign %%i %%i+1
