d9658bf0551f3fd37a6711520cbd44c0115624ab
quasilyte <quasilyte@gmail.com>
2016-09-06 16:52:59 +0300

    refactored inc ops to new macro

--- src/ops/inc.s
+++ src/ops/inc.s
@@ -23,16 +23,20 @@
   @gen_op inc_cx_by_r%1, {add CX, R%1}
 %endmacro
 
+%macro @inc_r?_by_imm? 2
+  consume_imm%2 rax
+  add R%1, rax
+%endmacro
+
 %macro @op_inc_r?_by_imm? 2
-  @@op_inc_r%1_by_imm%2:
-    consume_imm%2 rax
-    add R%1, rax
-    @next_op
+  @gen_op inc_r%1_by_imm%2, {@inc_r?_by_imm? %1, %2}
+%endmacro
+
+%macro @inc_cx_by_imm? 1
+  consume_imm%1 rax
+  add CX, rax
 %endmacro
 
 %macro @op_inc_cx_by_imm? 1
-  @@op_inc_cx_by_imm%1:
-    consume_imm%1 rax
-    add CX, rax
-    @next_op
+  @gen_op inc_cx_by_imm%1, {@inc_cx_by_imm? %1}
 %endmacro
