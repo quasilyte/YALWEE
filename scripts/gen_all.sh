39b2410b5e87b1b610b4162bbca850574d679c68
quasilyte <quasilyte@gmail.com>
2016-09-06 14:09:07 +0300

    fixing gen. scripts

--- scripts/gen_all.sh
+++ scripts/gen_all.sh
@@ -2,7 +2,7 @@
 
 op_enum_path='tmp/op_enum.txt'
 s_op_labels='src/generated/op_labels.s'
-# s_op_sizes='src/generated/op_sizes.s'
+s_op_sizes='src/generated/op_sizes.s'
 c_mnemonics='include/c_api/mnemonics.h'
 
 ruby ./scripts/gen_op_enum.rb > $op_enum_path
@@ -10,8 +10,8 @@ ruby ./scripts/gen_op_enum.rb > $op_enum_path
 echo "generating $s_op_labels"
 awk -f scripts/op_enum_to_labels.awk "$op_enum_path" > "$s_op_labels"
 
-# echo "generating $s_op_sizes"
-# awk -f scripts/op_enum_to_sizes.awk "$op_enum_path" > "$s_op_sizes"
+echo "generating $s_op_sizes"
+awk -f scripts/op_enum_to_sizes.awk "$op_enum_path" > "$s_op_sizes"
 
 echo "generating $c_mnemonics"
 awk -f scripts/op_enum_to_mnemonics.awk "$op_enum_path" > "$c_mnemonics"
--- scripts/op_enum_to_sizes.awk
+++ scripts/op_enum_to_sizes.awk
@@ -2,8 +2,10 @@ BEGIN {
   print ";; this file was generated, do not edit by hand"
 }
 
-/^for_*/ || /^jump_*/ || /.*imm(8|32|64).*/ {
-  print "qd 0 ;; "$0; 
+/^jump_*/ || /.*imm(8|32|64).*/ || 
+/^for_*/ || /break_for/ || /end_for_nz/ ||
+/^prepare*/ || /^exec*/ {
+  print "dq 0 ;; "$0; 
   next
 }
 
