08f28366834b48825a9138d8ba124555c8c6d1a8
quasilyte <quasilyte@gmail.com>
2016-09-07 23:10:08 +0300

    gen op with begin+end

--- src/macro/op_def.s
+++ src/macro/op_def.s
@@ -5,3 +5,14 @@
     @@op_%1_end:
       @next_op
 %endmacro
+
+%macro @op_begin 1
+  %define .op_name %1
+  @@op_%1:
+    @@op_%1_begin:
+%endmacro
+
+%macro @op_end 0
+    @@op_%[.op_name]_end:
+      @next_op
+%endmacro
