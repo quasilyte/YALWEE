362801d33ba0496021f839389be8b157ef95899d
quasilyte <quasilyte@gmail.com>
2016-09-12 17:02:16 +0300

    removing old scripts

--- maker/assert.rb
+++
@@ -1,9 +0,0 @@
-def blame op_prefix, arg, domains
-  raise "#{op_prefix} should belong to #{domains.inspect}; \"#{arg.name}\" given"
-end
-
-def assert what, arg, domains, op_name = caller[0]
-  unless domains.include? arg.name
-    blame "#{op_name}: #{what}", arg, domains
-  end
-end
--- maker/include.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'arg.rb'
-require_relative 'assert.rb'
-require_relative 'arg_source.rb'
-require_relative 'opcode.rb'
-require_relative 'opcode_source.rb'
-require_relative 'code_generator.rb'
-require_relative 'asm_spec.rb'
